57601af139d5c09feb481c6b2985c710096b5bf5
Contrean <contrean12@gmail.com>
2020-06-11 16:27:39 +0200

    finished module

---
+++ __init__.py
@@ -0,0 +1,39 @@
+import random
+
+def testNumber(number):
+    primenumber = True
+
+    for i in range(2, number-1):
+        if number % i == 0:
+            primenumber = False
+
+    if number < 2:
+        primenumber = False
+    
+    return primenumber
+
+def getPrimenumbers(end, start = 2): #0 and 1 aren't primenumbers, primebumbers can't be negative
+    
+    primenumbers = []
+    for i in range(end - start + 1):
+        isPrimenumber = testNumber(i + start)
+        if isPrimenumber:
+            primenumbers.append(i + start)
+    
+    return primenumbers
+
+def getPrimenumberstate(end, start = 2): #0 and 1 aren't primenumbers, primebumbers can't be negative
+    primenumbers = {}
+    for i in range(end - start + 1):
+        isPrimenumber = testNumber(i + start)
+        primenumbers[i + start] = isPrimenumber
+    
+    return primenumbers
+
+def getRandomPrimenumber(end = 1024, start = 2):
+    primenumbers = getPrimenumbers(end, start)
+    if primenumbers == []:
+        raise f"getRandomPrimenumbers(end = {end}, start = {start}) >>> No Primenumbers Found!"
+    else:
+        randPrime = random.choice(primenumbers)
+        return randPrime
